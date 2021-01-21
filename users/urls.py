5e8cabf89cd818b66ab0a0584f77af64cf4fb16b
Асхат Арифуллов <arifullov73@icloud.com>
2021-01-21 23:36:14 +0400

    [6.3] Добавил временные ссылки с реализованным функционалом на домашнюю страницу.

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from users.views import CustomLoginView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='account_login'),
     path('', include('allauth.urls')),
 ]
